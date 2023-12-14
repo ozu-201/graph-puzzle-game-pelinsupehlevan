2641247b266b52df121154cc10cd47b9b9a9b9d4
pelinsupehlevan <pelinsu.pehlevan@ozu.edu.tr>
2023-12-14 15:39:29 +0300

    project 3 - week 1

---
+++ cmake-build-debug/CMakeFiles/graph-puzzle-game-pelinsupehlevan.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/graph-puzzle-game-pelinsupehlevan.dir/src/main.cpp.obj"
+  "CMakeFiles/graph-puzzle-game-pelinsupehlevan.dir/src/main.cpp.obj.d"
+  "libgraph-puzzle-game-pelinsupehlevan.a"
+  "libgraph-puzzle-game-pelinsupehlevan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/graph-puzzle-game-pelinsupehlevan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by pp028364 on 12/14/2023.
+//
+
+#include <iostream>
+
+int main () {
+    std::cout << "Hello world!";
+
+    return 0;
+}
